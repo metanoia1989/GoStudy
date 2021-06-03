25a357cb69aada561eae6ca65d36e8bd291aacb5
刘深海 <m15171641694@163.com>
2021-06-03 18:02:47 +0800

    feat: 添加文件上传

--- src/web/04_Process_Request/parse-form.go
+++ src/web/04_Process_Request/parse-form.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -24,12 +25,37 @@ func formValue(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "(4)", r.MultipartForm)
 }
 
+func processFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
+}
+
+// http.Request.FormFile 处理单个文件，返回给定键的第一个值
+func singleFile(w http.ResponseWriter, r *http.Request) {
+	file, _, err := r.FormFile("uploaded")
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
+}
+
 func main() {
 	server := http.Server{
 		Addr: "0.0.0.0:8080",
 	}
 	http.HandleFunc("/process", process)
 	http.HandleFunc("/processMultiPart", processMultiPart)
+	http.HandleFunc("/processFile", processFile)
+	http.HandleFunc("/singleFile", singleFile)
 	http.HandleFunc("/formvalue", formValue)
 	server.ListenAndServe()
 }
